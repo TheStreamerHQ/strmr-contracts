4ba645c1eb924b63261823df8b6310e765c49c5e
Bohdan Khorolets <bogdan@khorolets.com>
2021-12-06 19:03:47 +0200

    Create subscription, list subscription of the specified user methods added

--- src/public.rs
+++ src/public.rs
@@ -7,6 +7,7 @@ impl Contract {
     pub fn buy_ft_tokens(&mut self) {
         let attached_deposit = env::attached_deposit();
         let signer_account_id = env::signer_account_id();
+        self.token.internal_register_account(&signer_account_id);
         // Calculate how many ft_tokens signer can get in exchange for the attached_deposit
         let affordable_amount: u128 = attached_deposit / self.exchange_price_in_yocto_near.0;
 
@@ -26,5 +27,37 @@ impl Contract {
             &self.metadata.get().unwrap().symbol,
             surplus,
         );
+    }
+
+    pub fn create_subscription(
+        &mut self,
+        endpoint: String,
+        event: subscriptions::Event,
+    ) {
+        let signer_account_id = env::signer_account_id();
+        let mut subscription_list = self.get_or_create_user_subscription_list(&signer_account_id);
+        let subscription_id = env::random_seed().get(0).unwrap().to_string();
+        let new_user_subscription = subscriptions::UserSubscription {
+            id: subscription_id.to_string(),
+            enabled: true,
+            endpoint,
+            event,
+        };
+        subscription_list.push(new_user_subscription);
+        self.subscriptions.insert(&signer_account_id, &subscription_list);
+    }
+}
+
+
+impl Contract {
+    fn get_or_create_user_subscription_list(&mut self, account_id: &AccountId) -> Vec<subscriptions::UserSubscription> {
+        match self.subscriptions.get(account_id) {
+            Some(subscription_list) => subscription_list,
+            None => {
+                let subscription_list: Vec<subscriptions::UserSubscription> = Vec::new();
+                self.subscriptions.insert(account_id, &subscription_list);
+                subscription_list
+            }
+        }
     }
 }
--- src/view.rs
+++ src/view.rs
@@ -7,4 +7,12 @@ impl Contract {
     pub fn exchange_price(&self) -> U128 {
         self.exchange_price_in_yocto_near
     }
+
+    pub fn subscriptions_of(&self, account_id: AccountId) -> Vec<subscriptions::UserSubscription> {
+        if let Some(subscriptions) = self.subscriptions.get(&account_id) {
+            // TODO: add pagination
+            return subscriptions
+        }
+        vec![]
+    }
 }
